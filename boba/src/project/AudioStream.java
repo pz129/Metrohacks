18c50f9678967ed6632af92445323de144ffe5fd
mango <stancepat@yahoo.com>
2018-05-19 07:11:08 -0400

    some stuff

---
+++ boba/src/project/AudioStream.java
@@ -0,0 +1,33 @@
+package project;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+
+@WebSocket
+public class AudioStream {
+    private static final Map<Session,String> sessions = new ConcurrentHashMap<Session,String>();
+
+    @OnWebSocketConnect
+    public void connected(Session session) {
+        sessions.put(session,"asdf");
+    }
+
+    @OnWebSocketClose
+    public void closed(Session session, int statusCode, String reason) {
+        sessions.remove(session);
+    }
+
+    @OnWebSocketMessage
+    public void message(Session session, String message) throws IOException {
+        System.out.println("Got: " + message);   // Print message
+        session.getRemote().sendString(sessions.get(session));
+    }
+}
--- boba/src/project/Site.java
+++ boba/src/project/Site.java
@@ -1,12 +1,12 @@
 package project;
 
-import org.jtwig.parser.*;
-import static spark.Spark.before;
-
-import spark.Route;
-import spark.Spark;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.websocket.server.WebSocketHandler;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import spark.Spark;
 public class Site {
 	public static final transient Logger log=LoggerFactory.getLogger(Site.class);
 	public static void main(String[] args) {
@@ -20,11 +20,28 @@ public class Site {
 		createRoutes();
 	}
 	protected void createRoutes() {
+		makeWebSocket();
 		Spark.get("/", new HomeController(this));
 		Spark.get("/about-us", new AboutUsController());
 		Spark.get("/dataset", new DataSetController());
 		Spark.post("/",new HomeController(this));
 		downloadGet("adsf.txt");
+	}
+	protected void makeWebSocket() {
+		Server server = new Server(8080);
+        WebSocketHandler wsHandler = new WebSocketHandler() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.register(AudioStream.class);
+            }
+        };
+        server.setHandler(wsHandler);
+        try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	protected void downloadGet(String filename) {
 		Spark.get("/download/"+filename, new DownloadController(filename));
